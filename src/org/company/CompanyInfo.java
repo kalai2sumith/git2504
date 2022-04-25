4e7fb9d0252a717f95b7756ecc1118ea560f98e4
kalai2sumith <kalaiskky92@gmail.com>
2022-04-25 18:09:53 +0530

    git2504

---
+++ src/org/company/CompanyInfo.java
@@ -0,0 +1,26 @@
+package org.company;
+
+public class CompanyInfo {
+	private void CompanyName() {
+		System.out.println("company name nanotech");
+	}
+	private void CompanyId() {
+		System.out.println("company Id is 14525254");
+
+	}
+	private void CompanyAddress() {
+	System.out.println("company address is chennai");
+
+	}
+public static void main(String[] args) {
+	CompanyInfo cc=new CompanyInfo();
+	cc.CompanyAddress();
+	cc.CompanyId();
+cc.CompanyName();
+		
+
+
+	}
+	}
+	
+
